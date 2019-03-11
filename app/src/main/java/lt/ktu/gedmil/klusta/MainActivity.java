28ce14842b39038db62f14ca7bc349ddb69c3a00
Gediminas <8672277+GedasFX@users.noreply.github.com>
2019-03-11 16:51:16 +0200

    Redesign base app

--- app/src/main/java/lt/ktu/gedmil/klusta/MainActivity.java
+++ app/src/main/java/lt/ktu/gedmil/klusta/MainActivity.java
@@ -1,5 +1,6 @@
 package lt.ktu.gedmil.klusta;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -27,8 +28,7 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_SHORT)
-                        .setAction("Action", null).show();
+                startActivity(new Intent());
             }
         });
 
@@ -80,18 +80,8 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.nav_manage) {
+            startActivity(new Intent(this, SettingsActivity.class));
         }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
